db13c0563e68625c2e4b15fd886f8d229bc09466
André Bittencourt <andre.bittencourt2890@gmail.com>
2023-07-13 00:09:53 +0200

    move user configuration to src/config

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 import 'dotenv/config'
 import Grabber from './src/classes/Grabber.js'
-import custom from './custom.js'
+import custom from './src/config/custom.js'
+import options from './src/config/options.js'
 
 const main = async () => {
-	let grabber = new Grabber()
+	let grabber = new Grabber(options)
 	grabber = custom(grabber)
 	await grabber.grab()
 }
---
+++ src/config/options.js
@@ -0,0 +1,3 @@
+export default {
+	// your puppeteer options here
+}
