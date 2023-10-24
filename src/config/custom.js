7b309d55d2556bc2686879d614c4e71f4ff5a2f9
André Bittencourt <andre.bittencourt2890@gmail.com>
2023-10-24 18:34:50 +0200

    customize grabber by reference

--- src/config/custom.js
+++ src/config/custom.js
@@ -1,7 +1,7 @@
+// eslint-disable-next-line no-unused-vars
 export default (grabber) => {
 	// add custom actions here
 	// grabber.addCustomAction('myCustomAction', async (brain, page) => {
 	// 	// do something
 	// })
-	return grabber
 }
